d6d98cdbde9c27d3a6bc0ad0f74e74aebc00c03d
SrChilde <kenny05yt@gmail.com>
2025-07-02 17:51:04 -0600

    con el Menú

---
+++ resources/views/indexglobal.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Principal</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+  </head>
+  <body class="pt-8 bg-white">
+
+    <div class="container mx-auto px-4">
+      <h1 class="text-center text-4xl font-bold text-gray-900 mb-6">Proyecto Ciclismo</h1>
+
+      <div class="text-center mb-6">
+        <div class="inline-flex rounded-md shadow-xs flex-wrap gap-1">
+          <a href="/ciclista" class="px-4 py-2 text-sm font-medium text-black bg-[#3692d9] border border-gray-200 rounded-s-lg hover:bg-[#2d7fc0] focus:ring-2 focus:ring-blue-700 focus:z-10">
+            Ciclistas
+          </a>
+          <a href="/nacionalidad" class="px-4 py-2 text-sm font-medium text-black bg-[#3692d9] border border-gray-200 hover:bg-[#2d7fc0] focus:ring-2 focus:ring-blue-700 focus:z-10">
+            Nacionalidades
+          </a>
+          <a href="/equipo" class="px-4 py-2 text-sm font-medium text-black bg-[#3692d9] border border-gray-200 hover:bg-[#2d7fc0] focus:ring-2 focus:ring-blue-700 focus:z-10">
+            Equipos
+          </a>
+          <a href="/nombrepruebas" class="px-4 py-2 text-sm font-medium text-black bg-[#3692d9] border border-gray-200 hover:bg-[#2d7fc0] focus:ring-2 focus:ring-blue-700 focus:z-10">
+            Nombre de pruebas
+          </a>
+          <a href="/director" class="px-4 py-2 text-sm font-medium text-black bg-[#3692d9] border border-gray-200 hover:bg-[#2d7fc0] focus:ring-2 focus:ring-blue-700 focus:z-10">
+            Directores
+          </a>
+          <a href="/prueba" class="px-4 py-2 text-sm font-medium text-black bg-[#3692d9] border border-gray-200 hover:bg-[#2d7fc0] focus:ring-2 focus:ring-blue-700 focus:z-10">
+            Pruebas
+          </a>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
